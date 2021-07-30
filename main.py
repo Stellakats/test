a6a122466317245f08659f572c7610b2a4caa881
Stellakats <stykat@kth.se>
2021-07-30 23:24:44 +0200

    no filter

--- main.py
+++ main.py
@@ -15,22 +15,7 @@ run = neptune.init(project='stella2/testing',
 image_size = (180, 180)
 batch_size = 32
 
-num_skipped = 0
-for folder_path in ("/Users/admnin/Desktop/PetImages/Cat", "/Users/admnin/Desktop/PetImages/Dog"):
-    for fname in os.listdir(folder_path):
-        fpath = os.path.join(folder_path, fname)
-        try:
-            fobj = open(fpath, "rb")
-            is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
-        finally:
-            fobj.close()
 
-        if not is_jfif:
-            num_skipped += 1
-            # Delete corrupted image
-            os.remove(fpath)
-
-print("Deleted %d images" % num_skipped)
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     "/Users/admnin/Desktop/PetImages",
